cee11620925dfd3e41af428a4f91218461efb9a9
ViktorJJF <vj.jimenez96@gmail.com>
2022-08-02 21:57:25 -0500

    added new array to main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ my_numbers = [1, 2, 3, 4]
 print([x**2 for x in my_numbers if x % 2 == 0])
 
 my_file = open("./new_file.txt", "w")
-# some new
+# new array
 my_file.write(
     json.dumps(
         [
+            {"name": "Victor", "lastname": "Jimenez"},
             {"name": "Victor", "lastname": "Jimenez"},
         ]
     )
